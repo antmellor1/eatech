c389da8db62d79048baceb074fc09db7cd8a8c00
Anthony Mellor <anthony.mellor@open-money.co.uk>
2023-05-14 13:54:30 +0100

    Add startup initialisation

--- EaTech.WebApi/Program.cs
+++ EaTech.WebApi/Program.cs
@@ -5,15 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<INetworkService, NetworkService>();
 
+builder.Services.AddHttpClient();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 app.UseSwagger();
+
+app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapHealthChecks("/health");
     endpoints.MapControllers();
-    endpoints.MapControllerRoute("default", "{controller=App}/{action=Index}");
 });
 
 app.Run();
