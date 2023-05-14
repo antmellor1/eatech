b37fdc34ba90db2eec7dc615e1f83b062cc4ed86
Anthony Mellor <anthony.mellor@open-money.co.uk>
2023-05-14 13:54:22 +0100

    Add implementation

--- EaTech.Core/Services/NetworkService.cs
+++ EaTech.Core/Services/NetworkService.cs
@@ -1,13 +1,52 @@
 ﻿using EaTech.Core.DTOs;
+using EaTech.Core.Models;
 using EaTech.Core.Services.Interfaces;
-
 namespace EaTech.Core.Services
 {
     public class NetworkService : INetworkService
     {
         public DownstreamCustomerResponseDTO GetDownstreamCustomers(DownstreamCustomerRequestDTO request)
         {
-            throw new NotImplementedException();
+            var downstreamNodes = GetDownstreamNodes(request.SelectedNode, request.Network.Branches);
+
+            var customerNodes = request.Network.Customers
+                .Where(c => downstreamNodes.Contains(c.Node))
+                .ToList();
+
+            var totalCustomers = customerNodes.Sum(n => n.NumberOfCustomers);
+
+            return new DownstreamCustomerResponseDTO
+            {
+                TotalCustomers = totalCustomers,
+                Nodes = customerNodes
+            };
         }
+
+        private HashSet<int> GetDownstreamNodes(int startNode, IEnumerable<Branch> branches) 
+        {
+            var visitedNodes = new HashSet<int>();
+
+            var stack = new Stack<int>();
+            stack.Push(startNode);
+
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                visitedNodes.Add(node);
+
+                var children = branches
+                    .Where(b => b.StartNode == node)
+                    .Select(b => b.EndNode)
+                    .ToList();
+
+                foreach (var child in children)
+                {
+                    stack.Push(child);
+                }
+            }
+
+            return visitedNodes;
+        }
+    
     }
 }
